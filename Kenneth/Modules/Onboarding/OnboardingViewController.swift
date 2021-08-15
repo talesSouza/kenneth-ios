2040e5a41ff468db1d180aa08dd2d12d69cd2fe6
Bruno Moreira <>
2021-08-15 17:06:46 -0300

    Added page control to SliderViewController

--- Kenneth/Modules/Onboarding/OnboardingViewController.swift
+++ Kenneth/Modules/Onboarding/OnboardingViewController.swift
@@ -9,16 +9,4 @@ extension OnboardingViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        self.navigationController?.isNavigationBarHidden = true
-        if let font = UIFont(name: "SFProDisplay-Semibold", size: 16) {
-            self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: font]
-        }
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        self.navigationController?.isNavigationBarHidden = false
-        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
-    }
 }
--- Kenneth/Modules/Slider/SliderViewController.swift
+++ Kenneth/Modules/Slider/SliderViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class SliderViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet private var startButton: UIButton!
+    @IBOutlet private var pageControl: UIPageControl!
+}
+
+// MARK: - Life Cycle
+extension SliderViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+}
 
+// MARK: - ScrollViewDelegate
+extension SliderViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        self.view.layoutIfNeeded()
+        if scrollView.contentOffset.y != 0 {
+            scrollView.contentOffset.y = 0
+        }
+        updatePageControll(scrollView: scrollView)
+    }
+    
+    private func updatePageControll(scrollView: UIScrollView) {
+        
+        let scrollX = scrollView.contentOffset.x
+        let percent = scrollX / scrollView.contentSize.width
 
+        if percent < 0.25 {
+            pageControl.currentPage = 0
+        } else if percent < 0.50 {
+            pageControl.currentPage = 1
+        } else if percent < 0.75 {
+            pageControl.currentPage = 2
+            UIView.animate(withDuration: 0.5) {
+                self.startButton.alpha = 0
+            }
+        } else {
+            pageControl.currentPage = 3
+            UIView.animate(withDuration: 0.5) {
+                self.startButton.alpha = 1
+            }
+        }
+    }
 }
 
