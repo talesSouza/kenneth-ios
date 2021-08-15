9b235aee195840cc0d39f92cb1a73d2751ea2497
Tales Silveira de Souza <tales-silveira@hotmail.com>
2021-08-15 11:08:45 -0300

    Created CocoaTouchClass for viewControllers

---
+++ Kenneth/Modules/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+}
+
+// MARK: - Life Cycle
+extension OnboardingViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = false
+    }
+}
